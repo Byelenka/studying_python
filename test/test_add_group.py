6b7f09fa9b885888789ba5701efcb77de4672706
byelenka <byelenka@gmail.com>
2021-03-12 18:23:16 +0200

    Class for group methods is created

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -13,12 +13,12 @@ def app(request):
     
 def test_add_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="wercew", header="ecrwe", footer="wecrw"))
+    app.group.create(Group(name="wercew", header="ecrwe", footer="wecrw"))
     app.session.logout()
 
 
 def test_add_empty_group(app):
     app.session.login(username="admin", password="secret")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
 

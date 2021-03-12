3da2dc4de0d0480dc8164fc1c9df04c02927bfd2
byelenka <byelenka@gmail.com>
2021-03-12 19:44:03 +0200

    Test for contact editing is created

---
+++ test/test_edit_contact.py
@@ -0,0 +1,13 @@
+from model.contact import Contact
+
+
+def test_edit_first_contact(app):
+    app.session.login(username="admin", password="secret")
+    app.contact.edit_first_contact(Contact(firstname="First1", middlename="Middle1", lastname="Last1", nickname="Nickname1",
+                               title="Title1", company="Company1", address="address", home_phone="123",
+                               mobile_phone="222",
+                               work_phone="333", fax="444", email="1@test.com", email2="2@test.com",
+                               email3="3@test.com",
+                               homepage="google.com", bday="2", bmonth="February", byear="1998",
+                               address2="secondary address", phone2="456", notes="some text1"))
+    app.session.logout()

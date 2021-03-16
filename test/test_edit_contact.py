6baf40b9a773a193c38e18c434315a036b8e190d
byelenka <byelenka@gmail.com>
2021-03-16 15:10:41 +0200

    Preconditions for deletion and editing of groups and contacts are added

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -2,12 +2,18 @@ from model.contact import Contact
 
 
 def test_edit_contact_firstname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="testname"))
     app.contact.edit_first_contact(Contact(firstname="First1"))
 
 
 def test_edit_contact_lastname(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="testname"))
     app.contact.edit_first_contact(Contact(lastname="Last1"))
 
 
 def test_edit_contact_birthmonth(app):
+    if app.contact.count() == 0:
+        app.contact.create(Contact(firstname="testname"))
     app.contact.edit_first_contact(Contact(bmonth="February"))

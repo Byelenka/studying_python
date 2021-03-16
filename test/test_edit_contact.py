0d50bc29beefc128c0da2c746b092f6896b38786
byelenka <byelenka@gmail.com>
2021-03-16 14:41:22 +0200

    Methods for creation and editing contacts are modified

--- test/test_edit_contact.py
+++ test/test_edit_contact.py
@@ -1,11 +1,13 @@
 from model.contact import Contact
 
 
-def test_edit_first_contact(app):
-    app.contact.edit_first_contact(Contact(firstname="First1", middlename="Middle1", lastname="Last1", nickname="Nickname1",
-                               title="Title1", company="Company1", address="address", home_phone="123",
-                               mobile_phone="222",
-                               work_phone="333", fax="444", email="1@test.com", email2="2@test.com",
-                               email3="3@test.com",
-                               homepage="google.com", bday="2", bmonth="February", byear="1998",
-                               address2="secondary address", phone2="456", notes="some text1"))
+def test_edit_contact_firstname(app):
+    app.contact.edit_first_contact(Contact(firstname="First1"))
+
+
+def test_edit_contact_lastname(app):
+    app.contact.edit_first_contact(Contact(lastname="Last1"))
+
+
+def test_edit_contact_birthmonth(app):
+    app.contact.edit_first_contact(Contact(bmonth="February"))

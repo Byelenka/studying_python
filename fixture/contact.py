fd7c9b121b93e6557ce3d7ac216cce65b2ba3493
byelenka <byelenka@gmail.com>
2021-03-19 11:36:09 +0200

    Method get_contact_list and test_phones_on_home_page are modified for verification of all phones without splitting

--- fixture/contact.py
+++ fixture/contact.py
@@ -108,10 +108,9 @@ class ContactHelper:
                 last_name = cells[1].text
                 first_name = cells[2].text
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
-                all_phones = cells[5].text.splitlines()
+                all_phones = cells[5].text
                 self.contact_cache.append(Contact(firstname=first_name, lastname=last_name, id=id,
-                                                  home_phone=all_phones[0], mobile_phone=all_phones[1],
-                                                  work_phone=all_phones[2], phone2=all_phones[3]))
+                                                  all_phones_from_home_page=all_phones))
         return list(self.contact_cache)
 
     def open_contact_to_edit_by_index(self, index):
